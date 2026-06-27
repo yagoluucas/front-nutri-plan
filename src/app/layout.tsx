952a02a33b3795db40a50bd0f601439792cc4794
yagoluucas <yago543@gmail.com>
2026-06-27 16:16:17 -0300

    feat: add Vercel Analytics and implement base RootLayout with metadata and toast provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -50,6 +51,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
+        <Analytics />
         <Toaster richColors position="top-right" />
       </body>
     </html>
